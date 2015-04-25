7bc212641412c73b65d3f3a72716e8031e79f870
Moritz Biersack <m.biersack@arcor.de>
2015-04-25 11:46:47 +0200

    static update2

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -100,7 +100,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = 'Drone_Rental/static/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
